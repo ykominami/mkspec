53f6cfc339866a0772afbbc6db1ce0a12142a5eb
yasuo kominami <ykominami@gmail.com>
2021-06-02 13:39:00 +0900

    chagen extract_yaml to extract_in_yaml_file

--- lib/mkspec/util.rb
+++ lib/mkspec/util.rb
@@ -1,15 +1,20 @@
 # frozen_string_literal: true
 
 module Mkspec
+  require 'pp'
+
   class Util
     class << self
       def make_spec_filename(name)
         %(#{name}_spec.rb)
       end
 
-      def extract_yaml(yaml)
-        hash = YAML.load_file(yaml)
-        content = File.readlines(yaml).map { |l| l.chomp }.join("\n")
+      def get_file_content(file_path)
+        File.readlines(file_path).select{ |l| l !~ /^(\s*)#/ }.map { |l| l.chomp }.join("\n")
+      end
+
+      def extract_with_eruby(content, hash)
+        result = nil
         eruby = Erubis::Eruby.new(content)
         begin
           result = eruby.result(hash)
@@ -18,11 +23,43 @@ module Mkspec
             #{e.message}
             #{e.backtrace}
           ]
-          Loggerxcm.error_b do
+          pp message
+          Loggerxcm.fatal_b do
             #{message}
           end
         end
-        YAML.load(result)
+        result
+      end
+
+      def extract_in_yaml(yaml_str)
+        result = nil
+        hash = YAML.load(yaml_str)
+        #p "hash=#{hash}"
+        result = extract_with_eruby(yaml_str, hash)
+        #p "result=#{result}"
+        if result.instance_of?(String)
+          [YAML.load(result), result]
+        else
+          [nil , result]
+        end
+      end
+
+      def extract_in_yaml_all(yaml_str)
+        hash , result = extract_in_yaml(yaml_str)
+        hs = hash.find{ |key, value| value =~ /<%=/}
+        while hs != nil
+          hash , result2 = extract_in_yaml(result)
+          hs = hash.find{ |key, value| value =~ /<%=/}
+          result = result2
+        end
+        hs
+      end
+
+      def extract_in_yaml_file(yaml_file_path)
+        result = nil
+        #hash = YAML.load_file(yaml_file_path)
+        content = get_file_content(yaml_file_path)
+        extract_in_yaml(content)
       end
     end
   end
