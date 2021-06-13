10b050b37087211c471899451beb497ab8d98f9a
yasuo kominami <ykominami@gmail.com>
2021-06-13 19:34:09 +0900

    display variable name

--- bin/setup.bash
+++ bin/setup.bash
@@ -4,10 +4,19 @@ bin_abspath=$(cd $(dirname $0) && pwd)
 top_abspath=$(dirname ${bin_abspath})
 parent_abspath=$(dirname ${top_abspath})
 
-GLOBAL_YAML_FNAME=${parent_abspath}/mkspec_data/global.yml
-if [ -e ${GLOBAL_YAML_FNAME} ]; then
-	echo "export GLOBAL_YAML_FNAME=${GLOBAL_YAML_FNAME}"
+MKSPEC_GLOBAL_YAML_FNAME=${parent_abspath}/mkspec_data/global.yml
+MKSPEC_SPECIFIC_YAML_FNAME=${parent_abspath}/mkspec_data/specific.yml
+
+if [ -e ${MKSPEC_GLOBAL_YAML_FNAME} ]; then
+	echo "export MKSPEC_GLOBAL_YAML_FNAME=${MKSPEC_GLOBAL_YAML_FNAME}"
 else
 	echo ""
 fi
 
+if [ -e ${MKSPEC_SPECIFIC_YAML_FNAME} ]; then
+	echo "export MKSPEC_SPECIFIC_YAML_FNAME=${MKSPEC_SPECIFIC_YAML_FNAME}"
+else
+	echo ""
+fi
+
+echo "export MKSPEC_LOG_DIR=${PWD}/logs"
