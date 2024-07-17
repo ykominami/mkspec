5eaa9c847ef16e7310405210dcfb94e3e1151353
ykominami <ykominami@gmail.com>
2024-07-17 11:09:12 +0900

    add test_data and test_output directories

---
+++ test_data/test/_output/c_spec.rb
@@ -0,0 +1,135 @@
+require "spec_helper"
+require "aruba/rspec"
+require "pathname"
+
+RSpec.describe "tecsgen command", type: :aruba do
+  let(:test_case_parent_dir) { "test_case" }
+  let(:test_case_parent_dir_pn) { Pathname.new(test_case_parent_dir) }
+  let(:conf) { Mkspec::TestConf.new(ENV["MKSPEC_SPECIFIC_YAML_FNAME"], ENV["MKSPEC_GLOBAL_YAML_FNAME"], "mkspec", "tecsmerge", "/home/ykominami/repo_ykominami/mkspec_data") }
+  let(:o) { conf.o }
+  let(:original_output_dir_pn) { o.original_output_dir_pn }
+  let(:original_output_dir) { original_output_dir_pn.to_s }
+  let(:target_parent_dir_pn) { o.target_parent_dir_pn }
+  let(:inc_dir) { o.spec_test_test_include_dir_pn }
+  let(:dq) { %s!\"\"! }
+
+  context "singleton" do
+    before(:each) {
+      abs_test_case_parent_dir_pn = target_parent_dir_pn.join(test_case_parent_dir_pn)
+      target_dir_pn = abs_test_case_parent_dir_pn.join("singleton")
+      @cmdline = Clitest::Cmdline.new(nil, nil, target_dir_pn, "tecsgen")
+    }
+
+    def make_cmdline_1(test_case_dir, *file_list)
+      option_extra = []
+
+      @cmdline.make_cmdline_1(test_case_dir, o.result, option_extra.join(" "), file_list)
+    end
+
+    context "singleton_1" do
+      before(:each) {
+        cmdline = make_cmdline_1("1", "singleton.cdl")
+
+        run_command("bash #{cmdline}")
+      }
+      it "execute successfully", test_normal_sh: true, tc: 42 do expect(last_command_started).to be_successfully_executed end
+      it "don't have error in output", test_normal_sh_out: true, tc: 43 do expect(last_command_started).not_to have_output(/error:/) end
+    end
+  end
+  context "portarray" do
+    before(:each) {
+      abs_test_case_parent_dir_pn = target_parent_dir_pn.join(test_case_parent_dir_pn)
+      target_dir_pn = abs_test_case_parent_dir_pn.join("portarray")
+      @cmdline = Clitest::Cmdline.new(nil, nil, target_dir_pn, "tecsgen")
+    }
+
+    def make_cmdline_1(test_case_dir, *file_list)
+      option_extra = []
+
+      @cmdline.make_cmdline_1(test_case_dir, o.result, option_extra.join(" "), file_list)
+    end
+
+    context "portarray_1" do
+      before(:each) {
+        cmdline = make_cmdline_1("1", "portarray.cdl")
+
+        run_command("bash #{cmdline}")
+      }
+      it "execute successfully", test_normal_sh: true, tc: 44 do expect(last_command_started).to be_successfully_executed end
+      it "don't have error in output", test_normal_sh_out: true, tc: 45 do expect(last_command_started).not_to have_output(/error:/) end
+    end
+  end
+  context "cmoposite_1" do
+    before(:each) {
+      abs_test_case_parent_dir_pn = target_parent_dir_pn.join(test_case_parent_dir_pn)
+      target_dir_pn = abs_test_case_parent_dir_pn.join("cmoposite_1")
+      @cmdline = Clitest::Cmdline.new(nil, nil, target_dir_pn, "tecsgen")
+    }
+
+    def make_cmdline_1(test_case_dir, *file_list)
+      option_extra = []
+
+      @cmdline.make_cmdline_1(test_case_dir, o.result, option_extra.join(" "), file_list)
+    end
+
+    context "cmoposite_1_1" do
+      before(:each) {
+        cmdline = make_cmdline_1("1", "cmoposite_1.cdl")
+
+        run_command("bash #{cmdline}")
+      }
+      it "execute successfully", test_normal_sh: true, tc: 46 do expect(last_command_started).to be_successfully_executed end
+      it "don't have error in output", test_normal_sh_out: true, tc: 47 do expect(last_command_started).not_to have_output(/error:/) end
+    end
+  end
+  context "recursive_composite" do
+    before(:each) {
+      abs_test_case_parent_dir_pn = target_parent_dir_pn.join(test_case_parent_dir_pn)
+      target_dir_pn = abs_test_case_parent_dir_pn.join("recursive_composite")
+      @cmdline = Clitest::Cmdline.new(nil, nil, target_dir_pn, "tecsgen")
+    }
+
+    def make_cmdline_1(test_case_dir, *file_list)
+      option_extra = []
+
+      @cmdline.make_cmdline_1(test_case_dir, o.result, option_extra.join(" "), file_list)
+    end
+
+    context "recursive_composite_1" do
+      before(:each) {
+        cmdline = make_cmdline_1("1", "recursive_composite.cdl")
+
+        run_command("bash #{cmdline}")
+      }
+      it "execute successfully", test_normal_sh: true, tc: 48 do expect(last_command_started).to be_successfully_executed end
+      it "don't have error in output", test_normal_sh_out: true, tc: 49 do expect(last_command_started).not_to have_output(/error:/) end
+    end
+    context "recursive_composite_2" do
+      before(:each) {
+        cmdline = make_cmdline_1("2", "recursive_composite.cdl")
+
+        run_command("bash #{cmdline}")
+      }
+      it "execute successfully", test_normal_sh: true, tc: 50 do expect(last_command_started).to be_successfully_executed end
+      it "don't have error in output", test_normal_sh_out: true, tc: 51 do expect(last_command_started).not_to have_output(/error:/) end
+    end
+    context "recursive_composite_2_idx_is_id" do
+      before(:each) {
+        cmdline = make_cmdline_1("2_idx_is_id", "recursive_composite.cdl")
+
+        run_command("bash #{cmdline}")
+      }
+      it "execute successfully", test_normal_sh: true, tc: 52 do expect(last_command_started).to be_successfully_executed end
+      it "don't have error in output", test_normal_sh_out: true, tc: 53 do expect(last_command_started).not_to have_output(/error:/) end
+    end
+    context "recursive_composite_3" do
+      before(:each) {
+        cmdline = make_cmdline_1("3", "recursive_composite.cdl")
+
+        run_command("bash #{cmdline}")
+      }
+      it "execute successfully", test_normal_sh: true, tc: 54 do expect(last_command_started).to be_successfully_executed end
+      it "don't have error in output", test_normal_sh_out: true, tc: 55 do expect(last_command_started).not_to have_output(/error:/) end
+    end
+  end
+end
---
+++ test_data/test/_output/p_spec.rb
@@ -0,0 +1,138 @@
+require "spec_helper"
+require "aruba/rspec"
+require "pathname"
+
+RSpec.describe "tecsgen command", type: :aruba do
+  let(:test_case_parent_dir) { "test_case" }
+  let(:test_case_parent_dir_pn) { Pathname.new(test_case_parent_dir) }
+  let(:conf) { Mkspec::TestConf.new(ENV["MKSPEC_SPECIFIC_YAML_FNAME"], ENV["MKSPEC_GLOBAL_YAML_FNAME"], "mkspec", "tecsmerge", "/home/ykominami/repo_ykominami/mkspec_data") }
+  let(:o) { conf.o }
+  let(:original_output_dir_pn) { o.original_output_dir_pn }
+  let(:original_output_dir) { original_output_dir_pn.to_s }
+  let(:target_parent_dir_pn) { o.target_parent_dir_pn }
+  let(:inc_dir) { o.spec_test_test_include_dir_pn }
+  let(:dq) { %s!\"\"! }
+
+  context "dynamic" do
+    before(:each) {
+      abs_test_case_parent_dir_pn = target_parent_dir_pn.join(test_case_parent_dir_pn)
+      target_dir_pn = abs_test_case_parent_dir_pn.join("dynamic")
+      @cmdline = Clitest::Cmdline.new(nil, nil, target_dir_pn, "tecsgen")
+    }
+
+    def make_cmdline_1(test_case_dir, *file_list)
+      option_extra = []
+
+      @cmdline.make_cmdline_1(test_case_dir, o.result, option_extra.join(" "), file_list)
+    end
+
+    context "dynamic_normal" do
+      before(:each) {
+        cmdline = make_cmdline_1("normal", "dynamic.cdl")
+
+        run_command("bash #{cmdline}")
+      }
+      it "execute successfully", test_normal_sh: true, tc: 186 do expect(last_command_started).to be_successfully_executed end
+      it "don't have error in output", test_normal_sh_out: true, tc: 187 do expect(last_command_started).not_to have_output(/error:/) end
+    end
+    context "dynamic_ram_initializer" do
+      before(:each) {
+        cmdline = make_cmdline_1("ram_initializer", "dynamic.cdl")
+
+        run_command("bash #{cmdline}")
+      }
+      it "execute successfully", test_normal_sh: true, tc: 188 do expect(last_command_started).to be_successfully_executed end
+      it "don't have error in output", test_normal_sh_out: true, tc: 189 do expect(last_command_started).not_to have_output(/error:/) end
+    end
+    context "dynamic_ramonly" do
+      before(:each) {
+        cmdline = make_cmdline_1("ramonly", "dynamic.cdl")
+
+        run_command("bash #{cmdline}")
+      }
+      it "execute successfully", test_normal_sh: true, tc: 190 do expect(last_command_started).to be_successfully_executed end
+      it "don't have error in output", test_normal_sh_out: true, tc: 191 do expect(last_command_started).not_to have_output(/error:/) end
+    end
+    context "dynamic_singleton" do
+      before(:each) {
+        cmdline = make_cmdline_1("singleton", "dynamic.cdl")
+
+        run_command("bash #{cmdline}")
+      }
+      it "execute successfully", test_normal_sh: true, tc: 192 do expect(last_command_started).to be_successfully_executed end
+      it "don't have error in output", test_normal_sh_out: true, tc: 193 do expect(last_command_started).not_to have_output(/error:/) end
+    end
+    context "dynamic_singleton_ramonly" do
+      before(:each) {
+        cmdline = make_cmdline_1("singleton_ramonly", "dynamic.cdl")
+
+        run_command("bash #{cmdline}")
+      }
+      it "execute successfully", test_normal_sh: true, tc: 194 do expect(last_command_started).to be_successfully_executed end
+      it "don't have error in output", test_normal_sh_out: true, tc: 195 do expect(last_command_started).not_to have_output(/error:/) end
+    end
+    context "dynamic_singleton_ram_initializer" do
+      before(:each) {
+        cmdline = make_cmdline_1("singleton_ram_initializer", "dynamic.cdl")
+
+        run_command("bash #{cmdline}")
+      }
+      it "execute successfully", test_normal_sh: true, tc: 196 do expect(last_command_started).to be_successfully_executed end
+      it "don't have error in output", test_normal_sh_out: true, tc: 197 do expect(last_command_started).not_to have_output(/error:/) end
+    end
+    context "dynamic_array" do
+      before(:each) {
+        cmdline = make_cmdline_1("array", "dynamic.cdl")
+
+        run_command("bash #{cmdline}")
+      }
+      it "execute successfully", test_normal_sh: true, tc: 198 do expect(last_command_started).to be_successfully_executed end
+      it "don't have error in output", test_normal_sh_out: true, tc: 199 do expect(last_command_started).not_to have_output(/error:/) end
+    end
+    context "dynamic_array_r" do
+      before(:each) {
+        cmdline = make_cmdline_1("array_r", "dynamic.cdl")
+
+        run_command("bash #{cmdline}")
+      }
+      it "execute successfully", test_normal_sh: true, tc: 200 do expect(last_command_started).to be_successfully_executed end
+      it "don't have error in output", test_normal_sh_out: true, tc: 201 do expect(last_command_started).not_to have_output(/error:/) end
+    end
+    context "dynamic_array_ram_initializer" do
+      before(:each) {
+        cmdline = make_cmdline_1("array_ram_initializer", "dynamic.cdl")
+
+        run_command("bash #{cmdline}")
+      }
+      it "execute successfully", test_normal_sh: true, tc: 202 do expect(last_command_started).to be_successfully_executed end
+      it "don't have error in output", test_normal_sh_out: true, tc: 203 do expect(last_command_started).not_to have_output(/error:/) end
+    end
+    context "dynamic_singleton_array" do
+      before(:each) {
+        cmdline = make_cmdline_1("singleton_array", "dynamic.cdl")
+
+        run_command("bash #{cmdline}")
+      }
+      it "execute successfully", test_normal_sh: true, tc: 204 do expect(last_command_started).to be_successfully_executed end
+      it "don't have error in output", test_normal_sh_out: true, tc: 205 do expect(last_command_started).not_to have_output(/error:/) end
+    end
+    context "dynamic_singleton_array_r" do
+      before(:each) {
+        cmdline = make_cmdline_1("singleton_array_r", "dynamic.cdl")
+
+        run_command("bash #{cmdline}")
+      }
+      it "execute successfully", test_normal_sh: true, tc: 206 do expect(last_command_started).to be_successfully_executed end
+      it "don't have error in output", test_normal_sh_out: true, tc: 207 do expect(last_command_started).not_to have_output(/error:/) end
+    end
+    context "dynamic_singleton_array_ram_initializer" do
+      before(:each) {
+        cmdline = make_cmdline_1("singleton_array_ram_initializer", "dynamic.cdl")
+
+        run_command("bash #{cmdline}")
+      }
+      it "execute successfully", test_normal_sh: true, tc: 208 do expect(last_command_started).to be_successfully_executed end
+      it "don't have error in output", test_normal_sh_out: true, tc: 209 do expect(last_command_started).not_to have_output(/error:/) end
+    end
+  end
+end
